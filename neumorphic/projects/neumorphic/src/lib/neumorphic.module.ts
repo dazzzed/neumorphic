d805e18d9a33da011a1692ff93cd3ea664924a4f
dazzzed <ptiago07@gmail.com>
2020-02-10 19:55:17 +0000

    Initial commit

---
+++ neumorphic/projects/neumorphic/src/lib/neumorphic.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from "@angular/core";
+
+import { ButtonComponent } from "./components/button/button.component";
+
+@NgModule({
+  declarations: [ButtonComponent, ButtonComponent],
+  imports: [],
+  exports: [ButtonComponent]
+})
+export class NeumorphicModule {}
---
+++ neumorphic/projects/neumorphic/src/public-api.ts
@@ -0,0 +1,6 @@
+/*
+ * Public API Surface of neumorphic
+ */
+
+export * from "./lib/components/button/button.component";
+export * from "./lib/neumorphic.module";
